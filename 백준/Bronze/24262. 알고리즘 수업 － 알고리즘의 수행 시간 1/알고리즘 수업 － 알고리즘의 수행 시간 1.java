64daa180b949a28e75037c8c46ca2ebba20ac837
seunghyun711 <87018064+seunghyun711@users.noreply.github.com>
2023-11-30 21:58:29 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,19 @@
+/*
+1. n 입력
+2. MenOfPassion 알고리즘은 배열과 n을 입력받아 배열의 특정 인덱스 값을 리턴하는 알고리즘이다
+따라서 O(1)의 시간 복잡도를 가진다.
+*/
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 1. n 입력
+        int n = Integer.parseInt(br.readLine());
+        
+        // 2. MenOfPassion 알고리즘은 배열과 n을 입력받아 배열의 특정 인덱스 값을 리턴하는 알고리즘이다
+        // 따라서 O(1)의 시간 복잡도를 가진다.
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
